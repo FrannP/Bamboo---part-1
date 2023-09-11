db0ac22880b76e7b91c86554c91c2754d45ae0e2
FrannP <franpinter20@gmail.com>
2023-09-11 13:05:05 +0200

    Add dropdwon fix

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 const hamburgerButton = document.querySelector(".hamburger");
 const dropdownMenu = document.querySelector(".drop-menu");
+const htmlOverflow = document.querySelector("html")
 
 hamburgerButton.addEventListener("click", () => {
     dropdownMenu.classList.toggle("drop-menu--dropped");
-    hamburgerButton.classList.toggle("hamburger--clicked"); 
+    hamburgerButton.classList.toggle("hamburger--clicked");
+    htmlOverflow.classList.toggle("overflow--hidden");
+    console.log("yo")
 
-
-  console.log("yo")
 });
